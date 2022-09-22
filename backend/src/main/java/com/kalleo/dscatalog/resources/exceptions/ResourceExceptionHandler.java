e99dff26c5d21f05b4b0eb1dd28daffcca492bf2
Kalleo Leandro dos Santos Leal <kalleo.leal@gmail.com>
2022-09-21 23:03:20 -0300

    Update category

--- backend/src/main/java/com/kalleo/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/kalleo/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.kalleo.dscatalog.services.exceptions.EntityNotFoundException;
+import com.kalleo.dscatalog.services.exceptions.ResourceNotFoundException;
 
 /**
  * @author Kalleo
@@ -22,8 +22,8 @@ import com.kalleo.dscatalog.services.exceptions.EntityNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		StandardError err = new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(), "Resource not found", e.getMessage(), request.getRequestURI());		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
